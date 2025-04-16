a457f03457a6788352d6a5ddb2b2160e19f76e1b
Yakau Bubnou <girokompass@gmail.com>
2025-04-16 22:36:16 +0200

    Add decoding implementation (missing reserved positions though).

--- test/test_bpe.cc
+++ test/test_bpe.cc
@@ -8,7 +8,7 @@
 using namespace metalchat;
 
 
-TEST_CASE("Test BPE open", "[bpe]")
+TEST_CASE("Test BPE encode and decode", "[bpe]")
 {
     bpe tokenizer("../Llama-3.2-1B/original/tokenizer.model");
 
@@ -16,6 +16,9 @@ TEST_CASE("Test BPE open", "[bpe]")
     REQUIRE(ids.size(0) == 6);
 
     std::vector<int32_t> actual(ids.begin(), ids.end());
-    std::vector<int32_t> expect = {2028, 374, 264, 1296, 11914, 13};
+    std::vector<bpe::index_type> expect = {2028, 374, 264, 1296, 11914, 13};
     REQUIRE_THAT(actual, Catch::Matchers::Equals(expect));
+
+    auto str = tokenizer.decode(ids.data_ptr(), ids.data_ptr() + ids.size(0));
+    REQUIRE(str == "This is a test sentence.");
 }
