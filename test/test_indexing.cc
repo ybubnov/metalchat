af34e444b1d11d09ee6f569f3624ffbda509eaf8
Yakau Bubnou <girokompass@gmail.com>
2025-04-16 22:36:06 +0200

    Replace boost testing library with `Catch2`.

--- test/test_indexing.cc
+++ test/test_indexing.cc
@@ -1,7 +1,4 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE indexing
-
-#include <boost/test/included/unit_test.hpp>
+#include <catch2/catch_test_macros.hpp>
 
 #include <metalchat/device.h>
 #include <metalchat/dtype.h>
@@ -13,26 +10,23 @@ using namespace metalchat::dtype;
 using namespace metalchat::indexing;
 
 
-BOOST_AUTO_TEST_SUITE(TestIndexing)
-
-
-BOOST_AUTO_TEST_CASE(tensor_slice)
+TEST_CASE("Read-write tensor slicing", "[tensor::operator]")
 {
     auto T = full<bf16>({4, 5}, 5.0);
     for (auto i = 0; i < T.size(0); i++) {
         for (auto j = 0; j < T.size(1); j++) {
-            BOOST_REQUIRE_EQUAL(T[i][j], 5.0);
+            REQUIRE(T[i][j] == 5.0);
             T[i][j] = bf16((i + 1) * 10 + j);
         }
     }
 
     auto S = T[slice(1, 3), slice(1, 4)];
-    BOOST_REQUIRE_EQUAL(S.size(0), 2);
-    BOOST_REQUIRE_EQUAL(S.size(1), 3);
+    REQUIRE(S.size(0) == 2);
+    REQUIRE(S.size(1) == 3);
 
-    BOOST_REQUIRE_EQUAL(S.stride(0), 5);
-    BOOST_REQUIRE_EQUAL(S.stride(1), 1);
-    BOOST_REQUIRE(!S.is_contiguous());
+    REQUIRE(S.stride(0) == 5);
+    REQUIRE(S.stride(1) == 1);
+    REQUIRE(!S.is_contiguous());
 
     for (auto i = 0; i < S.size(0); i++) {
         for (auto j = 0; j < S.size(1); j++) {
@@ -44,10 +38,7 @@ BOOST_AUTO_TEST_CASE(tensor_slice)
     // underlying storage.
     for (auto i = 1; i < 3; i++) {
         for (auto j = 1; j < 4; j++) {
-            BOOST_REQUIRE_EQUAL(T[i][j], 0.0);
+            REQUIRE(T[i][j] == 0.0);
         }
     }
 }
-
-
-BOOST_AUTO_TEST_SUITE_END()
