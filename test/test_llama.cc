6b6103c70cffcb9102921cfe5c03313edc91de08
Yakau Bubnou <girokompass@gmail.com>
2025-04-16 22:36:31 +0200

    Remove synchronous execution of BMM kernel.

--- test/test_llama.cc
+++ test/test_llama.cc
@@ -13,7 +13,7 @@ using namespace metalchat::dtype;
 
 template <typename T, ContiguousContainer Container>
 std::size_t
-argmax_(const tensor<T, 3, Container>& t)
+argmax_(shared_tensor<T, 3, Container> t)
 {
     std::size_t id = 0;
     auto last = t.size(1) - 1;
@@ -44,14 +44,14 @@ TEST_CASE("Test make model", "[llama]")
 
     auto input0 = to_tensor<int32_t>({1, ids.size()}, ids.begin(), ids.end());
     auto output0 = m(input0, 0);
-    auto id = argmax_(output0);
+    auto id = argmax_(output0.get());
     std::cout << input_text;
     std::cout << bpe.decode(id);
 
     for (auto i = input0.size(1); i < 16; i++) {
         auto input = full<int32_t>({1, 1}, id);
         auto output = m(input, i);
-        id = argmax_(output);
+        id = argmax_(output.get());
         std::cout << bpe.decode(id) << std::flush;
     }
     std::cout << std::endl;
