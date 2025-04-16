dbb702b31eb05c0a09fc4d947779b9b57ef3dc08
Yakau Bubnou <girokompass@gmail.com>
2025-04-16 22:36:42 +0200

    Migrate copy-kernels to the tensor2 class.

--- kernel/tensor.h
+++ kernel/tensor.h
@@ -63,3 +63,36 @@ template <typename T, uint N> struct tensor {
         return layout.offsets[dim];
     }
 };
+
+
+template <typename T> struct tensor2 {
+    constant tensor_layout<2>& layout;
+    device T* data;
+
+    inline device T&
+    at(uint i0, uint i1)
+    {
+        auto ptr_offset = 0;
+        ptr_offset += layout.strides[0] * i0 + layout.offsets[0];
+        ptr_offset += layout.strides[1] * i1 + layout.offsets[1];
+        return *(data + ptr_offset);
+    }
+
+    inline uint
+    size(uint dim)
+    {
+        return layout.sizes[dim];
+    }
+
+    inline uint
+    stride(uint dim)
+    {
+        return layout.strides[dim];
+    }
+
+    inline uint
+    offset(uint dim)
+    {
+        return layout.offsets[dim];
+    }
+};
