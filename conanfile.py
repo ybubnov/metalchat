a67c64d5e1a19946f52b54fb9840d330ff6c0bc6
Yakau Bubnou <girokompass@gmail.com>
2025-05-01 15:47:18 +0200

    Compile MacOS framework instead of a regular dynamic library.

---
+++ conanfile.py
@@ -0,0 +1,42 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.apple import XcodeToolchain, XcodeDeps
+
+
+class MetalChat(ConanFile):
+    name = "MetalChat"
+    description = "Llama inference for Apple Devices"
+    url = "https://github.com/ybubnov/metalchat"
+
+    package_type = "library"
+    settings = "os", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+
+    default_options = {
+        "shared": True,
+        "fPIC": True,
+        "catch2/*:shared": True,
+        "metal-cpp/*:shared": True,
+        "simdjson/*:shared": False,
+        "pcre2/*:shared": False,
+    }
+
+    def requirements(self):
+        self.requires("catch2/3.7.1")
+        self.requires("cppcodec/0.2")
+        self.requires("metal-cpp/15")
+        self.requires("simdjson/3.11.2")
+        self.requires("pcre2/10.44")
+
+    def generate(self):
+        cmake_deps = CMakeDeps(self)
+        cmake_deps.generate()
+
+        cmake_toolchain = CMakeToolchain(self)
+        cmake_toolchain.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
