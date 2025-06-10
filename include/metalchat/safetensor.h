a6da3686bf6a46f625003b2749cd5136fc81e29d
Yakau Bubnou <girokompass@gmail.com>
2025-06-10 17:15:58 +0200

    Move memory file management to a separate class.

--- include/metalchat/safetensor.h
+++ include/metalchat/safetensor.h
@@ -18,6 +18,78 @@
 namespace metalchat {
 
 
+class basic_memfile {
+public:
+    basic_memfile(const std::filesystem::path& p)
+    {
+        _m_file = std::fopen(p.c_str(), "r");
+        if (_m_file == nullptr) {
+            throw std::invalid_argument(std::format("unable to open file '{}'", p.string()));
+        }
+
+        _m_file_size = static_cast<std::size_t>(std::filesystem::file_size(p));
+        int fd = fileno(_m_file);
+        if (fd == -1) {
+            throw std::invalid_argument(
+                std::format("unable to get file descriptor for file '{}'", p.string())
+            );
+        }
+
+        _m_map = static_cast<uint8_t*>(mmap(nullptr, _m_file_size, PROT_READ, MAP_PRIVATE, fd, 0));
+        if (_m_map == MAP_FAILED) {
+            throw std::invalid_argument(
+                std::format("unable to memory-map safetensors file '{}'", p.string())
+            );
+        }
+    }
+
+    std::size_t
+    size() const noexcept
+    {
+        return _m_file_size;
+    }
+
+    basic_memfile&
+    read(void* dest, std::size_t size)
+    {
+        if (_m_file_size < _m_file_off + size) {
+            throw std::out_of_range(
+                std::format("file is too short ({}) to read ({}) more bytes", _m_file_size, size)
+            );
+        }
+
+        std::memcpy(dest, _m_map + _m_file_off, size);
+        _m_file_off += size;
+
+        return *this;
+    }
+
+    std::uint8_t*
+    tellp() const
+    {
+        return _m_map + _m_file_off;
+    }
+
+    ~basic_memfile()
+    {
+        if (_m_map != MAP_FAILED && _m_map != nullptr) {
+            munmap(_m_map, _m_file_size);
+            _m_map = nullptr;
+        }
+        if (_m_file != nullptr) {
+            std::fclose(_m_file);
+            _m_file = nullptr;
+        }
+    }
+
+private:
+    std::FILE* _m_file = nullptr;
+    std::size_t _m_file_size = 0;
+    std::size_t _m_file_off = 0;
+    std::uint8_t* _m_map = nullptr;
+};
+
+
 class safetensor {
 private:
     std::vector<std::size_t> _m_shape;
@@ -65,48 +137,15 @@ public:
     using const_iterator = std::unordered_map<std::string, safetensor>::const_iterator;
 
     safetensor_file(const std::filesystem::path& p)
+    : _m_memfile(std::make_shared<basic_memfile>(p))
     {
-        _m_file = std::fopen(p.c_str(), "r");
-        if (_m_file == nullptr) {
-            throw std::invalid_argument(std::format("unable to open file '{}'", p.string()));
-        }
-
-        _m_file_size = static_cast<std::size_t>(std::filesystem::file_size(p));
-        int fd = fileno(_m_file);
-        if (fd == -1) {
-            throw std::invalid_argument(
-                std::format("unable to get file descriptor for file '{}'", p.string())
-            );
-        }
-
-        _m_map = static_cast<uint8_t*>(mmap(nullptr, _m_file_size, PROT_READ, MAP_PRIVATE, fd, 0));
-        if (_m_map == MAP_FAILED) {
-            throw std::invalid_argument(
-                std::format("unable to memory-map safetensors file '{}'", p.string())
-            );
-        }
-
         parse();
     }
 
-    safetensor_file(safetensor_file&& other)
-    {
-        _m_file = other._m_file;
-        _m_file_size = other._m_file_size;
-        _m_file_off = other._m_file_off;
-        _m_map = other._m_map;
-        _m_tensors = other._m_tensors;
-
-        other._m_file_size = 0;
-        other._m_file_off = 0;
-        other._m_file = nullptr;
-        other._m_map = nullptr;
-    }
-
-    inline std::size_t
+    std::size_t
     size() const noexcept
     {
-        return _m_file_size;
+        return _m_tensors.size();
     }
 
     iterator
@@ -145,42 +184,12 @@ public:
         return _m_tensors.at(tensor_name);
     }
 
-    ~safetensor_file()
-    {
-        if (_m_map != MAP_FAILED && _m_map != nullptr) {
-            munmap(_m_map, _m_file_size);
-            _m_map = nullptr;
-        }
-        if (_m_file != nullptr) {
-            std::fclose(_m_file);
-            _m_file = nullptr;
-        }
-    }
-
 private:
-    std::FILE* _m_file = nullptr;
-    std::size_t _m_file_size = 0;
-    std::size_t _m_file_off = 0;
-
-    uint8_t* _m_map = nullptr;
-
+    std::shared_ptr<basic_memfile> _m_memfile;
     std::unordered_map<std::string, safetensor> _m_tensors;
 
     void
     parse();
-
-    void
-    memory_read(void* dest, std::size_t size)
-    {
-        if (_m_file_size < _m_file_off + size) {
-            throw std::out_of_range(
-                std::format("file is too short ({}) to read ({}) more bytes", _m_file_size, size)
-            );
-        }
-
-        std::memcpy(dest, _m_map + _m_file_off, size);
-        _m_file_off += size;
-    }
 };
 
 
