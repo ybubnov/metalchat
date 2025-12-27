87737a0169c43e08ecebbaef9b58c7d234e1e7e5
Yakau Bubnou <girokompass@gmail.com>
2025-12-27 21:18:19 +0100

    Link program with sqlite-cpp library.

--- program/config.h
+++ program/config.h
@@ -4,7 +4,7 @@
 
 #pragma once
 
-#include <toml/toml.h>
+#include <SQLiteCpp/SQLiteCpp.h>
 
 
 namespace metalchat {
--- program/main.cc
+++ program/main.cc
@@ -5,11 +5,12 @@
 #include <CLI/CLI.hpp>
 
 #include "chat.h"
+#include "config.h"
 #include "model.h"
 #include "remote.h"
 
 
-static const std::string config_path = "~/.metalchat/config";
+static const std::string program_path = ".metalchat";
 
 
 int
@@ -18,7 +19,6 @@ main(int argc, char** argv)
     std::string config_option;
 
     CLI::App app("A self-sufficient runtime for large language models", "metalchat");
-    app.add_option("--config", config_option)->default_val(config_path);
 
     metalchat::program::remote_command remote(app);
     metalchat::program::model_command model(app);
