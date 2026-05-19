2e613ee5c2494c7ff52b6a14df80135caf4d5196
Yakau Bubnou <girokompass@gmail.com>
2026-05-19 11:18:23 +0200

    Dynamically resolve version of the formula.

--- Formula/metalchat.rb
+++ Formula/metalchat.rb
@@ -1,22 +1,32 @@
 class Metalchat < Formula
+  def self.source_path
+    source_uri = URI.parse(url)
+    Pathname.new(source_uri.path).parent
+  end
+
+  def source_path
+    Metalchat::source_path
+  end
+
+  def self.resolve_version
+    File.read(source_path / "version.txt").strip
+  end
+
+  def self.resolve_url
+    URI::File.build(path: __dir__).to_s
+  end
+
   desc "Llama inference for Apple Silicon"
   homepage "https://metalchat.readthedocs.org"
   license "GPL-3.0-or-later"
-  url "file://#{source_path}"
+  url resolve_url
+  version resolve_version
 
   depends_on "conan@2" => :build
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "openssl@3"
   depends_on "curl"
-
-  def version
-    Version.new(File.read(source_path/"version.txt").strip)
-  end
-
-  def self.source_path
-    Pathname.new(__dir__).parent
-  end
 
   def install
     build_path = buildpath/"build"
@@ -28,7 +38,7 @@ class Metalchat < Formula
       --options use_system_libs=True
     ]
 
-    cp_r sourcepath, build_path
+    cp_r source_path, build_path
     system "conan", "profile", "detect"
     system "conan", "build", *build_args, build_path
 
