babdb55b07698a1e602355050a240555c202933b
Yakau Bubnou <girokompass@gmail.com>
2026-08-29 23:23:17 +0200

    Add `metalchat::chunk` method (#193)
    This patch adds a method `chunk` that slices the tensor along the
    specified dimentions into the chunks of equal size.

--- test/test_concatenate.cc
+++ test/test_concatenate.cc
@@ -5,6 +5,7 @@
 #include <ranges>
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_exception.hpp>
 
 
 #include <metalchat/accelerator.h>
@@ -42,4 +43,29 @@ TEST_CASE("Tensor concatenate", "[concatenate]")
     REQUIRE(output2.size(2) == 10);
     REQUIRE(output2[0, 0, 0] == 1.0);
     REQUIRE(output2[2, 3, 9] == 5.0);
+}
+
+
+TEST_CASE("Tensor chunk", "[chunk]")
+{
+    auto t0 = rand<float>({10, 20, 8});
+    auto chunks = chunk<4>(t0, /*dim=*/1);
+
+    for (auto& chunk : chunks) {
+        REQUIRE(chunk.size(0) == 10);
+        REQUIRE(chunk.size(1) == 5);
+        REQUIRE(chunk.size(2) == 8);
+    }
+}
+
+
+TEST_CASE("Tensor chunk not divisible", "[chunk]")
+{
+    auto t0 = rand<float>({10, 20, 8});
+
+    REQUIRE_THROWS_MATCHES(
+        chunk<3>(t0, /*dim=*/2), std::invalid_argument,
+        Catch::Matchers::Message("chunk: the tensor dimension (2) of size (8) is not "
+                                 "divisible by a number of chunks 3")
+    );
 }
